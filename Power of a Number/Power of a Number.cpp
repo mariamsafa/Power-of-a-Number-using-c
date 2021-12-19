18637881a8d1eb3ddb9cb6fa58574b0cb098e80d
Mariam Safa <88893709+mariamsafa@users.noreply.github.com>
2021-12-19 21:40:57 +0600

    pow function

--- Power of a Number/Power of a Number.cpp
+++ Power of a Number/Power of a Number.cpp
@@ -1,4 +1,4 @@
-#include<stdio.h>
+ #include<stdio.h>
 int main()
 {
 	int base, exp;
@@ -15,3 +15,21 @@ int main()
 	printf("Power of the number %d is %.2lf",base,result);
 	return 0;
 }
+
+//power of a number using pow function
+/*
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int base, exp;
+	double result ;
+	printf("Enter a base number : ");
+	scanf_s("%d", &base);
+	printf("Enter an exponent :");
+	scanf_s("%d", &exp);
+	result = pow(base, exp);
+	printf("%d^%d = %.2lf", base, exp, result);
+	return 0;
+}
+*/
